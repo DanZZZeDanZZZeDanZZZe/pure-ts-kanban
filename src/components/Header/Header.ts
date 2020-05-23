af27091eb4b186be6ef5c31c1993b4e661cdeb4b
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-05-23 15:52:59 +0300

    main components created

---
+++ src/components/Header/Header.ts
@@ -0,0 +1,26 @@
+import KanbanElement from "../../core/KanbanElement/KanbanElement";
+import { DOM } from "../../core/DomManipulator";
+
+export default class Header extends KanbanElement {
+  static className : string = 'header'
+
+  constructor($el : DOM) {
+    super($el)
+  }
+
+  getHTMLString() : string {
+    return htmlSring
+  }
+}
+
+const htmlSring : string = `
+  <div>
+    <h1>Kanban</h1>
+  </div>
+  
+  <div class="user-menu">
+    <button>
+      <i class="material-icons">all_inbox</i>        
+    </button>
+  </div>
+`
